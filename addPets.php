180a5d581274eae45b370429c9d45356abd82ce1
Angelica <angelicamld@gmail.com>
2025-08-25 17:46:06 +0200

    regex changement to string replace method, func ok

--- addPets.php
+++ addPets.php
@@ -14,25 +14,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($name_input)) {
         $nameErr = "Introduit un nom, champ obligatoire";
     } else {
-        $name = test_input($name_input);
+        $name = clean_input($name_input);
         // verifier d'autre facon
-        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+        if (!ctype_alpha(str_replace(' ', '', $name))) {
             $nameErr = "Format invalide";
         }
     }
     if (empty($description_input)) {
         $descriptionErr = "Rentre une description valide s'il te plait";
     } else {
-        $description = test_input($description_input);
+        $description = clean_input($description_input);
         // preg_match permet d'ajouter des accents et des caracteres speciaux // verifier d'autre facon
-        if (!preg_match("/^[a-zA-ZÀ-ÿ .,!?'-]*$/", $description)) {
+        if (!ctype_alpha(str_replace(' ', '', $description))) {
             $descriptionErr = "Format invalide";
         }
     }
     // if (empty($_FILES["image"])) {
     //     $imageErr = "Rentre une image valide s'il te plait";
     // } else {
-    //     $image = test_input($_FILES["image"]);
+    //     $image = clean_input($_FILES["image"]);
     // }
 
     // si aucune erreur, insert en bdd :
@@ -67,9 +67,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <h1>Ajoute ton pote aux 4 pattes ICI 🐶 🐱 🐰</h1>
+
     <form action="addPets.php" enctype="multipart/form-data" method="POST">
-        Quel est son nom : <input type="text" name="name" required><br>
-        Comment tu lui decris ? <input type="text" name="description" required><br>
+        Quel est son nom : <input type="text" name="name"><br>
+         <?php if (!empty($nameErr)): ?>
+        <span style="color: red;"><?= $nameErr ?></span><br>
+    <?php endif; ?>
+        Comment tu lui decris ? <input type="text" name="description"><br>
+        <?php if (!empty($descriptionErr)): ?>
+        <span style="color: red;"><?= $descriptionErr ?></span><br>
+    <?php endif; ?>
         <!-- Tu as des photos ? Vas y : <input type="file" name="image" accept="image/*"> -->
          <input name="button" type="submit">
     </form>
