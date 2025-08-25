27ef7ecdb7f963a5f44a4311800133b981d0e5c7
Angelica <angelicamld@gmail.com>
2025-08-25 17:47:51 +0200

    no changes

--- config.php
+++ config.php
@@ -1,21 +1,8 @@
 <?php
-// Example of the configuration file config.php
-$host_bdd = "localhost"; // Host of the database
-$user_bdd = "root"; // User of the database
-$pwd_bdd = ""; // Database password
+
+$host_bdd = "localhost";
+$user_bdd = "root"; 
+$pwd_bdd = "";
 $db_name = "mini-crud";
 
-function connectDb() {
-    global $host_bdd, $user_bdd, $pwd_bdd;
-        try {
-        $pdo = new PDO(
-            'mysql:host=' . $host_bdd . ';dbname=mini-crud', 
-            $user_bdd, 
-            $pwd_bdd);
-            return $pdo;
-        } catch (PDOException $e) {
-            print "Erreur !: " . $e->getMessage() . "<br/>";
-            die();
-        }
-}
 ?>
