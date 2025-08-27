4d9538c7a52262aee9326f74f39d1a7c2de89b2d
Angelica <angelicamld@gmail.com>
2025-08-27 17:49:12 +0200

    adding structure and stylesheets to inc header and footer, also an error stylesheet to global errors -> test it!

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <p>© 2025 Mate mon pote</p>
+    <p>Par la stagiaire de Superpictor, team Nat</p>
+</footer>
--- includes/header.php
+++ includes/header.php
@@ -1,23 +1,10 @@
-<?php
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="../style/header.css" type="text/css">
-</head>
-<body>
-    <nav>
-        <div class="header_logo">
-            <img src="/assets/images/logo.svg"/>
-            <h2>Mate mon pote</h2>
-        </div>
-        <div class="btn_container">
-            <button class="home_login_btn">Connexion</button>
-            <button class="home_login_btn">Inscription</button>
-        </div>
-    </nav>
-</body>
-</html>
+<nav>
+    <a href="../index.php" class="header_logo">
+        <img src="/assets/images/logo.svg"/>
+        <h2>Mate mon pote</h2>
+    </a>
+    <div class="btn_container">
+        <button class="home_login_btn">Connexion</button>
+        <button class="home_login_btn">Inscription</button>
+    </div>
+</nav>
