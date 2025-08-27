d78590679f74aced9f6220c3745e13033236ef8c
Angelica <angelicamld@gmail.com>
2025-08-27 12:32:04 +0200

    reorganizing sections and divs on body content

--- index.php
+++ index.php
@@ -20,43 +20,54 @@
 </head>
 <body>
     <div class="index_subtitle">
-        <p>L'amicale de potes aux 4 pattes 🐶 🐱 🐰</p><br>
+        <h2>L'amicale de potes aux 4 pattes 🐶 🐱 🐰</h2><br>
         <p>Comment ca marche ?</p>
         <img src="./assets/images/arrow.png"/>
     </div>
-    <section class="cards_bg">
-        <div class="card">
-            <div class="container">
-                <img src="./assets/images/profile_photo.png">
-                <p>1. Cree ton compte</p>
+    <main>
+        <section class="main_container">
+            <section class="cards_bg">
+                <div class="card">
+                    <div class="container">
+                        <img src="./assets/images/profile_photo.png">
+                        <p>1. Cree ton compte</p>
+                    </div>
+                </div> 
+                <div class="card">
+                    <div class="container">
+                        <img src="./assets/images/paw.png">
+                        <p>2. Ajoute ton pote</p>
+                    </div>
+                </div> 
+                <div class="card">
+                    <div class="container">
+                        <img src="./assets/images/ok-girl.png">
+                        <p>C'est OK ! Maintenant ton pote est en ligne.</p>
+                    </div>
+                </div> 
+            </section> 
+            <section class="pets_container">
+                <div>
+                    <img src="./assets/images/dogHome.png" />
+                    <img src="./assets/images/catHome.png" />
+                </div>
+            </section> 
+        </section>
+        <section class="pets_home_list">
+            <h3>Tous les potes</h3>
+            <div class="pets_cards">
+                <?php
+                    foreach($pets as $pet) { ?> 
+                        <div class="pet_card">
+                            <h4><?=$pet['pet_name'];?></h4>
+                            <p><?=$pet['description'];?></p>
+                            <a class="action-btn" href="./detailPets.php?id=<?=$pet['id'];?>">Connaitre ce pote</a>
+                        </div>
+                <?php } ?>
             </div>
-        </div> 
-        <div class="card">
-            <div class="container">
-                <img src="./assets/images/paw.png">
-                <p>2. Ajoute ton pote</p>
-            </div>
-        </div> 
-        <div class="card">
-            <div class="container">
-                <img src="./assets/images/ok-girl.png">
-                <p>C'est OK ! Maintenant ton pote est en ligne.</p>
-            </div>
-        </div> 
-    </section>  
-    <h2>Tous les potes</h2>
-    <?php
-        foreach($pets as $pet) { ?> 
-            <div>
-            <tr> 
-                <td><?=$pet['pet_name'];?></td><br>
-                <td><?=$pet['description'];?></td><br>
-                <button class="action-btn"><a href="./detailPets.php?id=<?=$pet['id'];?>">Connaitre ce pote</a></button>
-            </tr>
-            </div>
-        
-    <?php } ?>
-    <br>
-    <button class="action-btn"><a href="./addPets.php">Ajouter un pote 🐶 🐱 🐰</a></button><br>
+        </section>
+        <br>
+    </main>
+    <a class="action-btn" href="./addPets.php">Ajouter un pote 🐶 🐱 🐰</a><br>
 </body>
 </html>
