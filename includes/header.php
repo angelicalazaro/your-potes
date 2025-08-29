c5fe1f67f6a536002b49447652c4d9608e1bca41
Angelica <angelicamld@gmail.com>
2025-08-29 13:57:32 +0200

    fix sign up and login buttons: good redirections

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
         <h2>Mate mon pote</h2>
     </a>
     <div class="btn_container">
-        <a class="home_login_btn" href="../user/signUp.php">Connexion</a>
-        <a class="home_login_btn">Inscription</a>
+        <a class="home_login_btn" href="../user/login.php">Connexion</a>
+        <a class="home_login_btn" href="../user/signUp.php">Inscription</a>
     </div>
 </nav>
